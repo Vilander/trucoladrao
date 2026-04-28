748157049da74b5aebe4b1f1f406c1c97d80c556
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2026-04-27 22:23:35 -0300

    first-commit

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Hello Word</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    fontSize: 32,
+    color: '#fff',
+    backgroundColor: 'tomato',
+    padding: 26,
+    borderRadius: 8
+  }
+});
